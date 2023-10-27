c09227f3894a78675f022086361ff4d1fc8b006e
ralphordanza <mendelordanza@gmail.com>
2023-10-27 10:13:56 +0800

    check pre build

--- ios/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -4,7 +4,7 @@ echo "Stage: PRE-Xcode Build is activated .... "
 
 # Move to the place where the scripts are located.
 # This is important because the position of the subsequently mentioned files depend of this origin.
-cd $CI_PRIMARY_REPOSITORY_PATH/ci_scripts || exit 1
+cd $CI_WORKSPACE/ci_scripts || exit 1
 
 printf "{\n  \"OPENAI_API_KEY\": \"%s\",\n  \"BASE_URL\": \"https://api.openai.com/v1\",\n  \"API_URL\": \"https://umibpwlepdiwpledrlmx.supabase.co\",\n  \"ANON_KEY\": \"%s\",\n  \"APPLE_KEY\": \"%s\",\n  \"FACEBOOK_ID\": \"281988144520542\"\n}" "$OPENAI_API_KEY" "$BASE_URL" "$API_URL" "$ANON_KEY" "$APPLE_KEY" "$FACEBOOK_ID" >> ../config/app_config.json
 
