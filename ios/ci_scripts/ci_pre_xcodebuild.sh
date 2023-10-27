ac2296e45cd860900941a4923e56565475dbe76e
ralphordanza <mendelordanza@gmail.com>
2023-10-27 09:58:08 +0800

    update pre_xcodebuild

--- ios/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -4,7 +4,7 @@ echo "Stage: PRE-Xcode Build is activated .... "
 
 # Move to the place where the scripts are located.
 # This is important because the position of the subsequently mentioned files depend of this origin.
-cd $CI_WORKSPACE/ci_scripts || exit 1
+cd $CI_PRIMARY_REPOSITORY_PATH/ci_scripts || exit 1
 
 echo "Wrote Secrets.json file."
 
